d6e16c658a86fc84c56f32a2d9422e65e2f78cb1
Abemarkar23 <abemarkar23@gmail.com>
2019-04-26 14:51:48 -0700

    moved the GameStuff to the Appropriate file and added Neural Network

---
+++ NeuralNetwork/src/NeuralNetwork.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class NeuralNetwork {
+	int numInputs = 0;
+	
+	public static void main(String args[]) {
+		NeuralNetwork nn = new NeuralNetwork(5);
+	}
+	
+	ArrayList<ArrayList<Neuron>> layers = new ArrayList<ArrayList<Neuron>>();
+	
+	public NeuralNetwork(int numInputs) {
+		this.numInputs = numInputs;
+	}
+	
+	
+	/**
+	 * Adds a new layer to the network. No need to add the Input Layer
+	 *
+	 * @param  numNeurons - Number of Neurons
+	 * @param  activation - can choose between ReLu, Sigmoid, Tanh
+	 * @return void
+	 */
+	public void addLayer(int numNeurons, Activation activation) {
+		ArrayList<Neuron> newLayer = new ArrayList<Neuron>(); 
+		for (int i=0; i<numNeurons; i++) {
+			if (layers.size() == 1)
+				newLayer.add(new Neuron(activation, numInputs));
+			else
+				newLayer.add(new Neuron(activation, layers.get(layers.size()-1).size()));
+		}
+		layers.add(newLayer);
+	}
+	
+	
+}
