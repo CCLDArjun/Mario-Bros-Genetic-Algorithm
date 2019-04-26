3a5a613bea67a73b3b9313d8acdcc2790f4057f2
Abemarkar23 <abemarkar23@gmail.com>
2019-04-26 16:30:36 -0700

    added prediction function

--- NeuralNetwork/src/NeuralNetwork.java
+++ NeuralNetwork/src/NeuralNetwork.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
 
 public class NeuralNetwork {
-	int numInputs = 0;
-
+	private int numInputs = 0;
+	private double fitness = 0;
+	
+	
 	public static void main(String args[]) {
 		NeuralNetwork nn = new NeuralNetwork(5);
 	}
 
 	ArrayList<ArrayList<Neuron>> layers = new ArrayList<ArrayList<Neuron>>();
-
+	
+	
+	public void setFitness(double f) {
+		fitness = f;
+	}
+	
+	public double getFitness() {
+		return fitness;
+	}
+	
 	public NeuralNetwork(int numInputs) {
 		this.numInputs = numInputs;
 	}
@@ -30,5 +41,28 @@ public class NeuralNetwork {
 		}
 		layers.add(newLayer);
 	}
-
+	
+	public int predict(ArrayList<Double> input) {
+		ArrayList<Double> oldRes = input;
+		ArrayList<Double> newRes = new ArrayList<Double>();
+		
+		for (int r=0; r<layers.size(); r++) {
+			for (int c=0; c<layers.get(r).size(); c++) {
+				newRes.add(layers.get(r).get(c).propagate(oldRes));
+			}
+			oldRes = newRes;
+			newRes = new ArrayList<Double>();
+		}
+		return getMaxIndex(oldRes);
+	}
+	
+	public int getMaxIndex(ArrayList<Double> l) {
+		int lowestIndex = 0;
+		for (int i=1; i<l.size(); i++) {
+			if (l.get(i) < l.get(lowestIndex)) {
+				lowestIndex = i;
+			}
+		}
+		return lowestIndex;	
+	}
 }
