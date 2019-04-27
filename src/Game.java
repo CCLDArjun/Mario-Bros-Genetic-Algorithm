3b31aadb74d79c5a1b64c2f5c52de2fbd8c605e7
avighna <avighna.kukreja@gmail.com>
2019-04-27 13:13:32 -0700

    Made a moving Mario

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,85 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Game {
+	private JFrame frame = new JFrame("Ultra Mario Bros!");
+	private JPanel panel;
+	private Mario m = new Mario(0, 624);
+	private Keyboard keys = new Keyboard(m);
+	private int[][] tilelayout = new int[13][13];
+	private String[] tileID = {"AIR", "ground", "block"};
+//	private int[] tileData = {1, 2};
+	private Timer repaint = new Timer(1, new ActionListener(){
+		public void actionPerformed(ActionEvent e) {
+			frame.repaint();
+//				frame.dispose();
+//				JOptionPane.showMessageDialog(null, "You died!\nPoints: " + ((Integer) movey).toString(), "You lost!", JOptionPane.WARNINGMESSAGE);
+//				repaint.stop();
+//			}
+		}
+	});	
+	
+	public static void main(String[] args) {
+		new Game().start();
+	}
 
+	private void start() {
+		makeFrame();
+		repaint.start();
+	}
+
+	@SuppressWarnings("serial")
+	private void makeFrame() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel = new JPanel() {
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				m.key(keys.r);
+				draw(g);
+			}
+		};
+		frame.add(panel);
+		
+		for (int x = 0; x < tilelayout.length; x++) {
+			tilelayout[12][x] = 1;
+		}
+		
+		panel.repaint();
+		panel.setPreferredSize(new Dimension(624, 624));
+		panel.addKeyListener(keys);
+		panel.setFocusable(true);
+		panel.setLayout(null);
+		frame.pack();
+		frame.setVisible(true);
+
+	}
+
+	private void draw(Graphics g) {
+		for (int y = 0; y < tilelayout.length; y++) {
+			for (int x = 0; x < tilelayout.length; x++) {
+				int tile = tilelayout[y][x];
+				if (tile != 0) {
+					Image img;
+					try {
+						img = ImageIO.read(new File(tileID[tile] + ".png"));
+						g.drawImage(img, x * 48, y * 48, null);
+					} catch (IOException e) {
+						// Auto-generated catch block
+					}
+				}
+			}
+		}
+		
+		m.draw(g, tilelayout);
+	}
 }
