bc7f0223c4006396f725fb4f21d59aacf1bac285
c00lme <ksrivatsa19@gmail.com>
2019-05-02 21:52:44 -0700

    Update GeneticAlgorithm.java

--- GeneticAlgorithm/GeneticAlgorithm.java
+++ GeneticAlgorithm/GeneticAlgorithm.java
@@ -1,33 +1,64 @@
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 
 
 
-
-public class GeneticAlgorithm {
+public class GeneticAlgorithm implements Callable, Runnable {
+	private ArrayList <Individual> individuals =  new ArrayList <Individual>();
+	private double mutationRate;
+	private int popSize;
+	public NeuralNetwork n;
 	
-	public static void main(String []args) {
+	public GeneticAlgorithm(double mutationRate, int popSize, int numInputs) {
+		this.mutationRate = mutationRate; 
+		this.popSize = popSize;
+		for(int i = 0; i < popSize; i++) {
+			Individual ind = new Individual(numInputs);
+			individuals.add(new Individual(numInputs));
+			ind.play();
+			int index = findNextIndex();		
+		}
+	}
+	public static void MultiThreading() {
+		ExecutorService executor = Executors.newFixedThreadPool(10);
 		
-		Individual.generateUniqueID();
+		
+		for(int i = 0; i < 10; i++) {
+			Runnable worker = new GeneticAlgorithm(i); 
+			executor.execute(worker);
+			n.reproduce();
+		}
+		
+		executor.shutdown();
+		
+		
 	}
-	private ArrayList<Individual> population = new ArrayList<Individual>(25);
-	public GeneticAlgorithm(ArrayList<Individual> Individuals){
-
-		this.population = Individuals;
-
-	}
-
-
-
-	public void populate() {
 
 	
+	public int findNextIndex() {
+		return 1;
+	}
+	
+	
+	
+	@Override
+	public Object call() throws Exception {
 
+		return null;
 	}
 
 
-
-
-
+	@Override
+	public void run() {
+		
+	}
 
 }
