5b47aa5da4612218a8a124c3483ddcf1b0685753
Abemarkar23 <abemarkar23@gmail.com>
2019-05-20 09:44:57 -0700

    fixed NeuralNetwork

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
 
-public class NeuralNetwork {
+public class NeuralNetwork implements Serializable {
 	private int numInputs = 0;
 	private double fitness = 0;
 	private ArrayList<ArrayList<Neuron>> layers = new ArrayList<ArrayList<Neuron>>();
@@ -39,8 +41,8 @@ public class NeuralNetwork {
 		ArrayList<Double> oldRes = input;
 		ArrayList<Double> newRes = new ArrayList<Double>();
 		
-		for (int r = 0; r < layers.size(); r++) {
-			for (int c = 0; c < layers.get(r).size(); c++) {
+		for (int r=0; r<layers.size(); r++) {
+			for (int c=0; c<layers.get(r).size(); c++) {
 				newRes.add(layers.get(r).get(c).propagate(oldRes));
 			}
 			oldRes = newRes;
@@ -69,12 +71,12 @@ public class NeuralNetwork {
 			throw new RuntimeException("Mutation Rate given is not between 0 and 1 ");
 		
 		ArrayList<ArrayList<Neuron>> newLayers = new ArrayList<ArrayList<Neuron>>();
-		for (int r = 0; r < nn1.getLayers().size(); r++) {
+		for (int r=0; r<nn1.getLayers().size(); r++) {
 			newLayers.add(new ArrayList<Neuron>());
 		}
 		
-		for (int r = 0; r < nn1.getLayers().size(); r++) {
-			for (int c = 0; c < nn1.getLayers().get(r).size(); c++) {
+		for (int r=0; r<nn1.getLayers().size(); r++) {
+			for (int c=0; c<nn1.getLayers().get(r).size(); c++) {
 				Neuron n1 = nn1.getLayers().get(r).get(c);
 				Neuron n2 = nn2.getLayers().get(r).get(c);
 				newLayers.get(r).add(c, Neuron.reproduce(n1, n2, mutationRate));
@@ -101,6 +103,34 @@ public class NeuralNetwork {
 			maxIndexs.add(-1);
 		}
 		return maxIndexs;	
+	}
+	
+	public void save(String path) {
+		try {
+			FileOutputStream f = new FileOutputStream(path);
+			ObjectOutputStream out = new ObjectOutputStream(f);
+			out.writeObject(this);
+			out.close();
+			f.close();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static NeuralNetwork getFromFile(String path) throws EOFException {
+		try {
+			FileInputStream fi = new FileInputStream(path);
+			ObjectInputStream in;
+			in = new ObjectInputStream(fi);
+			NeuralNetwork net = (NeuralNetwork) in.readObject();
+			in.close();
+			fi.close();
+			return net;
+		} 
+		catch (Exception e) {
+		}
+		return null;
 	}
 	
 	
